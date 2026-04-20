576d31616e77d1b33ed8e3e0c8726f9d2af562a1
Dominic <deathmilex@gmail.com>
2026-04-20 01:20:51 -0500

    F

---
+++ Arce.Web.Api/Controllers/Proveedor/ProveedorController.cs
@@ -0,0 +1,85 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Arce.Web.Service;
+using Arce.Web.Entity.Proveedor;
+
+namespace MyApp.Namespace
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProveedorController : ControllerBase
+    {
+        public readonly IProveedorService _proveedorService;
+
+        public ProveedorController(IProveedorService proveedorService)
+        {
+            _proveedorService = proveedorService;
+        }
+        
+        [HttpGet]
+        [Route("getListarProveedorActivo")]
+        public async Task<IActionResult> ListarProveedorActivo(string Prv_Id, string Prv_Nom, string Prv_Ruc, string Prv_Nom_Con, string Flg_Est)
+        {
+            var result = await _proveedorService.ListarProveedorActivo(Prv_Id, Prv_Nom, Prv_Ruc, Prv_Nom_Con, Flg_Est);
+            if (result!.Success)
+            {
+                result.CodeResult = StatusCodes.Status200OK;
+                return Ok(result);
+            }
+
+            result.CodeResult = StatusCodes.Status400BadRequest;
+            return BadRequest(result);
+        }
+
+        [HttpPost]
+        [Route("postRegistrarProveedor")]
+        public async Task<IActionResult> RegistrarProveedor([FromBody] ProveedorEntity valores)
+        {
+            ProveedorEntity parametros = new ProveedorEntity
+            {
+                Prv_Nom = valores.Prv_Nom,
+                Prv_Ruc = valores.Prv_Ruc,
+                Prv_Tel = valores.Prv_Tel,
+                Prv_Dir = valores.Prv_Dir,
+                Prv_Nom_Con = valores.Prv_Nom_Con,
+                Usr_Reg = valores.Usr_Reg
+            };
+            
+            var result = await _proveedorService.RegistrarProveedor(parametros);
+            if (result!.Success)
+            {
+                result.CodeResult = StatusCodes.Status200OK;
+                return Ok(result);
+            }
+
+            result.CodeResult = StatusCodes.Status400BadRequest;
+            return BadRequest(result);
+        }
+
+        [HttpPatch]
+        [Route("patchActualizarProveedor")]
+        public async Task<IActionResult> ActualizarProveedor([FromBody] ProveedorEntity valores)
+        {
+            ProveedorEntity parametros = new ProveedorEntity
+            {
+                Prv_Id = valores.Prv_Id,
+                Prv_Nom = valores.Prv_Nom,
+                Prv_Ruc = valores.Prv_Tel,
+                Prv_Tel = valores.Prv_Dir,
+                Prv_Nom_Con = valores.Prv_Nom_Con,
+                Flg_Est = valores.Flg_Est,
+                Usr_Mod = valores.Usr_Mod
+            };
+            
+            var result = await _proveedorService.RegistrarProveedor(parametros);
+            if (result!.Success)
+            {
+                result.CodeResult = StatusCodes.Status200OK;
+                return Ok(result);
+            }
+
+            result.CodeResult = StatusCodes.Status400BadRequest;
+            return BadRequest(result);
+        }
+    }
+}
--- Arce.Web.Data/Proveedor/ProveedorRepository.cs
+++ Arce.Web.Data/Proveedor/ProveedorRepository.cs
@@ -2,39 +2,110 @@
 using System.Data.SqlClient;
 using System.Data;
 using Microsoft.Extensions.Configuration;
-using Arce.Web.Entity.ProveedorEntity;
+using Arce.Web.Entity.Proveedor;
 namespace Arce.Web.Data;
 
-public class ProveedorRepository
+public class ProveedorRepository: IProveedorRepository
 {
     private readonly string _connectionString;
 
     public ProveedorRepository(IConfiguration configuration)
     {
         _connectionString = configuration.GetConnectionString("Connection")!;
+    }
 
-        public async Task<IEnumerable<ProveedorEntity>?> ListarUsuarioActivo(string Usr_Id, string Usr_Cod, string Usr_Nom, string Flg_Est)
+    public async Task<IEnumerable<ProveedorEntity>?> ListarProveedorActivo(string Prv_Id, string Prv_Nom, string Prv_Ruc, string Prv_Nom_Con, string Flg_Est)
+    {
+        using (var connection = new SqlConnection(_connectionString))
         {
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                await connection.OpenAsync();
-
-                var parametros = new DynamicParameters();
-                parametros.Add("@Usr_Id", Usr_Id);
-                parametros.Add("@Usr_Cod", Usr_Cod);
-                parametros.Add("@Usr_Nom", Usr_Nom);
-                parametros.Add("@Flg_Est", Flg_Est);
-
-                var result = await connection.QueryAsync<ProveedorEntity>(
-                        "[dbo].[PA_Sg_Usuario_S0001]"
-                        , parametros
-                        , commandType: CommandType.StoredProcedure
-                );
-                return result;
-            }
+            await connection.OpenAsync();
+            var parametros = new DynamicParameters();
+            parametros.Add("@Prv_Id", Prv_Id);
+            parametros.Add("@Prv_Nom", Prv_Nom);
+            parametros.Add("@Prv_Ruc", Prv_Ruc);
+            parametros.Add("@Prv_Nom_Con", Prv_Nom_Con);
+            parametros.Add("@Flg_Est", Flg_Est);
+            var result = await connection.QueryAsync<ProveedorEntity>(
+                    "[dbo].[PA_Lg_Proveedor_S0001]"
+                    , parametros
+                    , commandType: CommandType.StoredProcedure
+            );
+            return result;
         }
+    }
 
+    public async Task<(int Codigo, string Mensaje)> RegistrarProveedor(ProveedorEntity valores)
+    {
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            await connection.OpenAsync();
+            var parametros = new DynamicParameters();
+            
+            parametros.Add("@Prv_Nom", valores.Prv_Nom);
+            parametros.Add("@Prv_Ruc", valores.Prv_Ruc);
+            parametros.Add("@Prv_Tel", valores.Prv_Tel);
+            parametros.Add("@Prv_Dir", valores.Prv_Dir);
+            parametros.Add("@Prv_Nom_Con", valores.Prv_Nom_Con);
+            parametros.Add("@Usr_Reg", valores.Usr_Reg);
+            parametros.Add("@Codigo", 0);
+            parametros.Add("@sMsj", "");
 
+            parametros.Add("@Codigo", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            parametros.Add("@sMsj", dbType: DbType.String, size: 255, direction: ParameterDirection.Output);
+            try
+            {
+                connection.Execute(
+                    "[dbo].[PA_Lg_Proveedor_I0001]"
+                    , parametros
+                    , commandType: CommandType.StoredProcedure
+                );
+            }
+            catch (SqlException ex)
+            {
+            }
+            var Codigo = parametros.Get<int>("@Codigo");
+            var mensaje = parametros.Get<string>("@sMsj");
+            return (Codigo, mensaje);
+        }
+    }
+
+    public async Task<(int Codigo, string Mensaje)> ActualizarProveedor(ProveedorEntity valores)
+    {
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            await connection.OpenAsync();
+
+            var parametros = new DynamicParameters();
+
+            parametros.Add("@Prv_Id", valores.Prv_Id);
+            parametros.Add("@Prv_Nom", valores.Prv_Nom);
+            parametros.Add("@Prv_Ruc", valores.Prv_Ruc);
+            parametros.Add("@Prv_Tel", valores.Prv_Tel);
+            parametros.Add("@Prv_Dir", valores.Prv_Dir);
+            parametros.Add("@Prv_Nom_Con", valores.Prv_Nom_Con);
+            parametros.Add("@Flg_Est", valores.Flg_Est);
+            parametros.Add("@Usr_Mod", valores.Usr_Mod);
+
+            parametros.Add("@Codigo", 0);
+            parametros.Add("@sMsj", "");
+
+            parametros.Add("@Codigo", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            parametros.Add("@sMsj", dbType: DbType.String, size: 255, direction: ParameterDirection.Output);
+            try
+            {
+                connection.Execute(
+                    "[dbo].[PA_Sg_Usuario_U0001]"
+                    , parametros
+                    , commandType: CommandType.StoredProcedure
+                );
+            }
+            catch (SqlException ex)
+            {
+            }
+            var Codigo = parametros.Get<int>("@Codigo");
+            var mensaje = parametros.Get<string>("@sMsj");
+            return (Codigo, mensaje);
+        }
     }
 
 }
