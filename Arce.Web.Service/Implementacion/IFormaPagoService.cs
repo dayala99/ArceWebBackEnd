9090c40fb3615aa5fc9186e339896464510d9112
Dominic <deathmilex@gmail.com>
2026-04-25 16:43:50 -0500

    Fin FormaPago

---
+++ Arce.Web.Service/Implementacion/IFormaPagoService.cs
@@ -0,0 +1,11 @@
+﻿using Arce.Web.Entity;
+using Arce.Web.Service.Comunes;
+
+namespace Arce.Web.Service;
+
+public interface IFormaPagoService
+{
+    Task<ServiceResponseList<FormaPagoEntity>?> ListarFormaPagoActivo(int? For_Pag_Id, string? For_Pag_Des, string? Flg_Est);
+    Task<ServiceResponse<int>> RegistrarFormaPago(FormaPagoEntity valores);
+    Task<ServiceResponse<int>> ActualizarFormaPago(FormaPagoEntity valores);
+}
