08c4f8e8ba2e0ef76a6f283a6983b4618724e908
Dominic <deathmilex@gmail.com>
2026-05-11 15:17:34 -0500

    Cambio string -> int 2 variables

--- Arce.Web.Entity/Pedido/PedidoDetalleEntity.cs
+++ Arce.Web.Entity/Pedido/PedidoDetalleEntity.cs
@@ -4,8 +4,8 @@ public class PedidoDetalleEntity
 {
     public int? Ped_Det_Id { get; set; }
     public int? Ped_Cab_Id { get; set; }
-    public string? Ped_Cod_Itm  { get; set; }
-    public string? Ped_Uni_Med { get; set; }
+    public int? Ped_Cod_Itm  { get; set; }
+    public int? Ped_Uni_Med { get; set; }
     public decimal? Ped_Can { get; set;}
     public decimal? Ped_Cos_Uni { get; set; }
     public decimal? Ped_Cos_Tot { get; set; }
